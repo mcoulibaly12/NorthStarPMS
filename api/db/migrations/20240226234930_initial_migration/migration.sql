804a899f66325754f5948f5d772fc018245719ac
mcoulibaly12 <pmsnorthstart@gmail.com>
2024-02-26 18:50:56 -0500

    use deployed db

--- api/db/migrations/20240226234930_initial_migration/migration.sql
+++ api/db/migrations/20240226234930_initial_migration/migration.sql
@@ -1,20 +1,55 @@
-/*
-  Warnings:
-
-  - You are about to drop the `PointOfContact` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'POC');
 
--- DropForeignKey
-ALTER TABLE "PointOfContact" DROP CONSTRAINT "PointOfContact_companyId_fkey";
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "addressOne" TEXT NOT NULL,
+    "addressTwo" TEXT,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "dunsNumber" INTEGER,
+    "cageNumber" INTEGER,
+    "websiteUrl" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
 
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "roles" "Role"[];
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
 
--- DropTable
-DROP TABLE "PointOfContact";
+-- CreateTable
+CREATE TABLE "Certificate" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "expirationDate" TIMESTAMP(3) NOT NULL,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SocioEconDesignation" (
+    "sedID" SERIAL NOT NULL,
+    "sedDescription" TEXT NOT NULL,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "SocioEconDesignation_pkey" PRIMARY KEY ("sedID")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "companyId" INTEGER,
+    "roles" "Role"[],
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Customer" (
@@ -73,6 +108,9 @@ CREATE TABLE "ProposalActivitity" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "CustomerPointOfContact_email_key" ON "CustomerPointOfContact"("email");
 
 -- CreateIndex
@@ -83,6 +121,15 @@ CREATE UNIQUE INDEX "Proposal_proposalCode_key" ON "Proposal"("proposalCode");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Proposal_custonmerId_key" ON "Proposal"("custonmerId");
+
+-- AddForeignKey
+ALTER TABLE "Certificate" ADD CONSTRAINT "Certificate_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SocioEconDesignation" ADD CONSTRAINT "SocioEconDesignation_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CustomerPointOfContact" ADD CONSTRAINT "CustomerPointOfContact_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
